3a203f9bbf73c15175b7c765f577438bd938b0e6
Pleba <lagt@seznam.cz>
2024-02-21 21:05:26 +0100

    sout failed, passed, percantage of tests

--- src/test/java/com/mycompany/app/TestResultWatcher.java
+++ src/test/java/com/mycompany/app/TestResultWatcher.java
@@ -22,7 +22,17 @@ public class TestResultWatcher implements TestWatcher, AfterAllCallback {
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
         TestResultContainer result  = getTestResultContainer();
-        System.out.println(result.getSuccessfulTests());
+        int failedTests = result.getFailures().size();
+        int passedTests  = result.getSuccessfulTests().size();
+        int runnedTestsNumber  = result.getFailures().size() + result.getSuccessfulTests().size();
+        float runnedSuccesfullPercentage = 0f;
+        if (runnedTestsNumber > 0 || passedTests > 0) {
+            runnedSuccesfullPercentage = (passedTests / runnedTestsNumber) * 100;
+        }
+        System.out.println("failed tests " + failedTests);
+        System.out.println("passed tests " + passedTests);
+        System.out.println("Total tests: " + runnedTestsNumber);
+        System.out.println("The success percentage is : " + runnedSuccesfullPercentage);
     }
 
     public TestResultContainer getTestResultContainer() {
