12ff2dc95620c0d51ebd1a48897ad1007b19b258
Pleba <lagt@seznam.cz>
2024-06-06 19:24:46 +0200

    dump/ comments

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import http from "http";
-import fs from "fs";
-import path from "path";
-import https from "https";
 import dotenv from "dotenv";
 import app from "./src/app.js";
 import { createClient } from "redis";
@@ -10,18 +7,10 @@ import { initTestQueue } from "./src/utils/testQueue.js";
 
 export let redisClient;
 
-/*
-let privateKey = fs.readFileSync("./ssl/privkey.pem", "utf8")
-let certificate = fs.readFileSync("./ssl/cert.pem", "utf8")
-let credentials = {key: privateKey, cert: certificate};
-console.log(credentials)
-*/
-
 dotenv.config({ path: "./src/.env" });
 let redisServer = createClient({url : process.env.REDIS_URL});
 
 const server = http.createServer(app);
-//const server = https.createServer(credentials,app);
 const PORT = process.env.PORT || 3000;
 
 async function appInit() {
