0bd5024764f2cff19231147c925063fb6def4677
Plebicek <lagt@seznam.cz>
2024-09-22 19:43:08 +0200

    remove src2

--- src2/web/index/entry/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import Logic from "./../logic/index.js"
-
-const router = Router()
-
-router.get("/", Logic.get)
-
-export default router
--- src2/web/index/logic/index.js
+++
@@ -1,5 +0,0 @@
-export default class IndexPage {
-    static get(req, res, next) {
-
-    }
-}
