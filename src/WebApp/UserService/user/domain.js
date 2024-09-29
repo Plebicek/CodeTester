b61ffb7fa8cf7ca7d86c57b2418f7250a91c1e1f
Plebicek <lagt@seznam.cz>
2024-09-29 21:14:41 +0200

    user domain add docs

--- src/WebApp/UserService/user/domain.js
+++ src/WebApp/UserService/user/domain.js
@@ -13,24 +13,44 @@ export class User {
         this.cookie = cookie //ToDo: map cookie data to the user object
         this.isAuth = true
     }
-
 }
 
 /**
  *  Client class based view  
+ *  @class Client 
  * */
 export default class Client {
+    /**
+     * Login page get method view 
+     * @static 
+     * @param {*} req 
+     * @param {*} res 
+     * @returns render login page
+     */
     static login(req, res) {
         console.log(req.baseUrl)
         return res.render("auth/login")
     }
 
+    /**
+     * Static logout logic 
+     * @static 
+     * @param {Request} _ 
+     * @param {Response} res 
+     * @returns redirect to homepage 
+     */
     static logout(_, res) {
         res.clearCookie("auth")
         return res.redirect("/")
     }
 
-
+    /**
+     * Basic password type auth logic 
+     * @static 
+     * @param {*} req 
+     * @param {*} res 
+     * @returns 
+     */
     static auth(req, res) {
         res.cookie("auth", { "user": true }, { maxAge: 900000, onlyHttp: true })
         return res.redirect("/")
