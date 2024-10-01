ed67ea0845b05c11a090afcfd4faf85827fd564a
Plebicek <lagt@seznam.cz>
2024-10-01 06:04:33 +0200

    add testing routes

---
+++ __tests__/WebApp/UserService/user/entry.test.js
@@ -0,0 +1,11 @@
+import agent from "superagent"
+import prefix from "superagent-prefix"
+const baseURL = "http://localhost:4000/"
+const superagent = agent.agent().use(prefix(baseURL))
+
+describe("/User/login", () => {
+    it("GET login response 200", async () => {
+        const res = await superagent.get("/user/login")
+        expect(res.status).toBe(200)
+    })
+})
